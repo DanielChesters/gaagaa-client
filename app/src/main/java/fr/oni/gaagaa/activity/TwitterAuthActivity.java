38c45f58d41f422bc6bd629396313af1ba6095a0
Daniel Chesters <daniel.chesters@gmail.com>
2014-11-25 23:44:49 +0100

    Start move to RxJava

--- app/src/main/java/fr/oni/gaagaa/activity/TwitterAuthActivity.java
+++ app/src/main/java/fr/oni/gaagaa/activity/TwitterAuthActivity.java
@@ -1,7 +1,6 @@
 package fr.oni.gaagaa.activity;
 
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
@@ -20,6 +19,12 @@ import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 import oauth.signpost.exception.OAuthNotAuthorizedException;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class TwitterAuthActivity extends ActionBarActivity {
     private static final String TAG = TwitterAuthActivity.class.getSimpleName();
@@ -30,6 +35,15 @@ public class TwitterAuthActivity extends ActionBarActivity {
 
     private OAuthProvider provider;
     private OAuthConsumer consumer;
+    private Subscription twitterWebAuthSubscription;
+    private Subscription twitterTokenSubscription;
+
+    @Override
+    protected void onDestroy() {
+        twitterWebAuthSubscription.unsubscribe();
+        twitterTokenSubscription.unsubscribe();
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,31 +71,31 @@ public class TwitterAuthActivity extends ActionBarActivity {
         }
     }
 
-    private void onOAuthCallback(final Uri uri) {
-        new AsyncTask<Void, Void, Void>() {
-
+    private void onOAuthCallback(Uri uri) {
+        Action1<Token> onNextAction = new Action1<Token>() {
             @Override
-            protected Void doInBackground(Void... params) {
-                String pinCode = uri.getQueryParameter("oauth_verifier");
-                try {
-                    provider.retrieveAccessToken(consumer, pinCode);
-                    String token = consumer.getToken();
-                    String tokenSecret = consumer.getTokenSecret();
-                    PrefUtil.setTwitterToken(TwitterAuthActivity.this, token);
-                    PrefUtil.setTwitterTokenSecret(TwitterAuthActivity.this, tokenSecret);
-                } catch (OAuthMessageSignerException | OAuthNotAuthorizedException
-                        | OAuthExpectationFailedException | OAuthCommunicationException e) {
-                    Log.e(TAG, e.getMessage(), e);
-                }
-                return null;
+            public void call(Token token) {
+                PrefUtil.setTwitterToken(TwitterAuthActivity.this, token.getKey());
+                PrefUtil.setTwitterTokenSecret(TwitterAuthActivity.this, token.getSecret());
             }
+        };
 
+        Action1<Throwable> onErrorAction = new Action1<Throwable>() {
             @Override
-            protected void onPostExecute(Void aVoid) {
+            public void call(Throwable throwable) {
+                Log.e(TAG, throwable.getMessage(), throwable);
+            }
+        };
+
+        Action0 onCompleteAction = new Action0() {
+            @Override
+            public void call() {
                 finish();
             }
-        }
-                .execute();
+        };
+
+        twitterTokenSubscription = getTwitterToken(uri).subscribe(onNextAction, onErrorAction,
+                onCompleteAction);
     }
 
     private void initTwitter() {
@@ -93,28 +107,84 @@ public class TwitterAuthActivity extends ActionBarActivity {
                 "https://api.twitter.com/oauth/authorize"
         );
 
-        new AsyncTask<Void, Void, Void>() {
-
+        Action1<String> onNextAction = new Action1<String>() {
             @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    authUrl = provider.retrieveRequestToken(consumer, CALLBACK);
-                    Log.d(TAG, "authUrl " + authUrl);
-                } catch (OAuthMessageSignerException | OAuthNotAuthorizedException
-                        | OAuthExpectationFailedException | OAuthCommunicationException e) {
-                    Log.e(TAG, e.getMessage(), e);
-                }
-                return null;
+            public void call(String s) {
+                authUrl = s;
             }
+        };
 
+        Action1<Throwable> onErrorAction = new Action1<Throwable>() {
             @Override
-            protected void onPostExecute(Void aVoid) {
+            public void call(Throwable throwable) {
+                Log.e(TAG, throwable.getMessage(), throwable);
+            }
+        };
+
+        Action0 onCompleteAction = new Action0() {
+            @Override
+            public void call() {
                 if (!TextUtils.isEmpty(authUrl)) {
                     webView.loadUrl(authUrl);
                 }
             }
-        }
-                .execute();
+        };
 
+        twitterWebAuthSubscription = getTwitterWebAuth()
+                .subscribe(onNextAction, onErrorAction, onCompleteAction);
+    }
+
+    private Observable<String> getTwitterWebAuth() {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                try {
+                    String authUrl = provider.retrieveRequestToken(consumer, CALLBACK);
+                    subscriber.onNext(authUrl);
+                    subscriber.onCompleted();
+                } catch (OAuthMessageSignerException | OAuthNotAuthorizedException
+                        | OAuthExpectationFailedException | OAuthCommunicationException e) {
+                    subscriber.onError(e);
+                }
+            }
+        }).subscribeOn(Schedulers.io());
+    }
+
+    private Observable<Token> getTwitterToken(final Uri uri) {
+        return Observable.create(new Observable.OnSubscribe<Token>() {
+
+            @Override
+            public void call(Subscriber<? super Token> subscriber) {
+                String pinCode = uri.getQueryParameter("oauth_verifier");
+                try {
+                    provider.retrieveAccessToken(consumer, pinCode);
+                    String token = consumer.getToken();
+                    String tokenSecret = consumer.getTokenSecret();
+                    subscriber.onNext(new Token(token, tokenSecret));
+                    subscriber.onCompleted();
+                } catch (OAuthMessageSignerException | OAuthNotAuthorizedException
+                        | OAuthExpectationFailedException | OAuthCommunicationException e) {
+                    subscriber.onError(e);
+                }
+            }
+        }).subscribeOn(Schedulers.io());
+    }
+
+    private static final class Token {
+        private String key;
+        private String secret;
+
+        private Token(String key, String secret) {
+            this.key = key;
+            this.secret = secret;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getSecret() {
+            return secret;
+        }
     }
 }
